9e4db10f9ea91c44979c7fa94bd8e87004ee670c
Timothy Rines <tsrines@gmail.com>
2019-08-15 02:34:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-var name = "Joe";
-var height = "70";
-
-var index = require("./index.js");
-
-if (name === "Susan"){
-  
-  console.log("The name is correct")
-}
-    else {
-      console.log("Expected the name to be Susan");
-    
-  
-  
+var name = "Joe"
+var height = "74"
+ 
+// Don't worry about this
+module.exports = { name, height
 }
 
 
